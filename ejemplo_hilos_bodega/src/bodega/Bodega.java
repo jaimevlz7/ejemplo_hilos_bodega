63b6b9b19efb21b919ac1fb2273cd631d86601d1
jaimevlz7 <jaime.velez2@correo.icesi.edu.co>
2016-04-20 08:22:18 -0500

    Update Bodega.java

--- ejemplo_hilos_bodega/src/bodega/Bodega.java
+++ ejemplo_hilos_bodega/src/bodega/Bodega.java
@@ -11,8 +11,8 @@ public class Bodega {
     int cantidadTipo2;
     int tipoArticulo;
     int volumen;
-    private final int volumenTipo1;
-    private final int volumenTipo2;
+    private int tipo1;
+    private int tipo2;
     public Semaphore semaforo;
 
     public Bodega() {
@@ -20,16 +20,19 @@ semaforo= new Semaphore(0);
         capacidadBodega = 0;
         cantidadTipo1 = 0;
         cantidadTipo2 = 0;
-        volumenTipo1 = 10;
-        volumenTipo2 = 15;
+        tipo1 = 10;
+        tipo2 = 15;
         this.volumen = volumen;
         this.tipoArticulo = tipoArticulo;
 
     }
 
     public void descargarArticulo(int tipoArticulo) throws InterruptedException {
+    	
+    	//Realizo validaciones por seguridad.
 
         if (tipoArticulo == TIPO1) {
+            //capacidad maxima 200, si tipo 1, como tipo 1 = 10 en tamaño, por eso menor  190.
             while(capacidadBodega > 190);
             semaforo.acquire();
             capacidadBodega = capacidadBodega + volumenTipo1;
@@ -39,17 +42,17 @@ semaforo= new Semaphore(0);
             semaforo.release();
         }
         if (tipoArticulo == TIPO2) {            
-            
+            //capacidad maxima 200, si tipo 2, como tipo 2 = 15 en tamaño, por eso menor  185.
             while(capacidadBodega > 185);
             semaforo.acquire();
-            capacidadBodega = capacidadBodega + volumenTipo2;
+            capacidadBodega = capacidadBodega + tipo2;
             semaforo.release();
             semaforo.acquire();
             cantidadTipo2++;
             semaforo.release();
         }
-        System.out.println(cantidadTipo1);
-        System.out.println(cantidadTipo2);
+        System.out.println("Hay" + cantidadTipo1 + "de tipo 1");
+        System.out.println("Hay" + cantidadTipo2 + "de tipo 2");
 		
     }
 
@@ -65,8 +68,8 @@ semaforo= new Semaphore(0);
 			semaforo.release();
 			System.out.println("Se ha empacado un articulo");
 			semaforo.acquire();
-			capacidadBodega = capacidadBodega - 3 * volumenTipo1 + 4 * volumenTipo2;
-        semaforo.release();
+			capacidadBodega = capacidadBodega - 3 * tipo1 + 4 * tipo2;
+			semaforo.release();
         }
 		
     }
